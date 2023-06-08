2380bd188c6a09966a21b56cfcb3f70136e4e2a6
NguyenPhuTai <phutai5704@gmail.com>
2023-06-08 09:31:53 +0700

    update create-acc

--- create-acc.php
+++ create-acc.php
@@ -52,7 +52,10 @@
                     <label for="">Email</label>
                     <input type="email" class="form-control" id="" placeholder="Input field">
                 </div>
-
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input type="text" class="form-control" id="" placeholder="Username">
+                </div>
 
 
                 <button type="submit" class="btn btn-primary">Submit</button>
