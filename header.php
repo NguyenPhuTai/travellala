4c294e7761ec0e6660ca838cc6a89e356d8c8fce
unknown <nguyenhuungocduonghd@gmail.com>
2023-06-13 10:15:45 +0700

    màu form đăng nhap

--- header.php
+++ header.php
@@ -162,12 +162,9 @@
                             <li>
                                 <div class="row">
                                         <div class="col-md-12">
-                                            Login via
-                                            <div class="social-buttons">
-                                                <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-                                                <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-                                            </div>
-                                            or
+                                            Login here
+                                            
+                                        
                                             <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
                                                     <div class="form-group">
                                                         <label class="sr-only" for="exampleInputEmail2">Email address</label>
@@ -176,16 +173,22 @@
                                                     <div class="form-group">
                                                         <label class="sr-only" for="exampleInputPassword2">Password</label>
                                                         <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                                        <!-- <div class="checkbox">
+                                                            <label>
+                                                                <input type="checkbox"> keep me logged-in
+                                                            </label>
+                                                        </div> -->
                                                         <div class="help-block text-right"><a href="">Forget the password ?</a></div>
                                                     </div>
                                                     <div class="form-group">
                                                         <button type="submit" class="btn btn-primary btn-block">Sign in</button>
                                                     </div>
-                                                    <div class="checkbox">
-                                                        <label>
-                                                        <input type="checkbox"> keep me logged-in
-                                                        </label>
+                                                    <p>or</p>
+                                                    <div class="social-buttons">
+                                                         <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                                                        <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
                                                     </div>
+                                                    
                                             </form>
                                         </div>
                                         <div class="bottom text-center">
