d7ed792f76fb2a1b3657c91b03f1ebacdffe98c0
NguyenPhuTai <Tai.NP220234P@sis.hust.edu.vn>
2023-06-18 13:39:49 +0700

    update map and display search airline

--- map.php
+++ map.php
@@ -47,57 +47,12 @@
     width: 350px;
     margin-left: 68%;
   }
+  .maps{
+    margin-top: 5%;
+    width: 1150px;
+    height: 600px;
+  }
 </style>
-<div class="container">
-  <h2>Carousel Example</h2>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-
-      <div class="item active">
-        <img src="photo/los.webp" alt="Los Angeles" style="width:100%; height: 400px;">
-        <div class="carousel-caption">
-          <h3>Los Angeles</h3>
-          <p>LA is always so much fun!</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="photo/chica.jpg" alt="Chicago" style="width:100%; height: 400px;">
-        <div class="carousel-caption">
-          <h3>Chicago</h3>
-          <p>Thank you, Chicago!</p>
-        </div>
-      </div>
-    
-      <div class="item">
-        <img src="photo/newyork.jpg" alt="New York" style="width:100%; height: 400px;">
-        <div class="carousel-caption">
-          <h3>New York</h3>
-          <p>We love the Big Apple!</p>
-        </div>
-      </div>
-  
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
 <div class="container main-box">
   <div class="box-header">
     <div>một chiều / khứ hồi</div>
@@ -159,6 +114,9 @@
     <button type="submit" class="search-airline">Tìm chuyến bay</button>
     </div>
   </form>
+</div>
+<div class="container">
+    <iframe class="maps" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29793.988211049866!2d105.8369637!3d21.022739599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1687069870243!5m2!1sen!2s"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 <?php
 include_once('footer.php');
