e4a191f71fc2367c408129d0f76f2a95f8008de3
NguyenPhuTai <Tai.NP220234P@sis.hust.edu.vn>
2023-06-20 07:19:04 +0700

    fix funtion

--- function.php
+++ function.php
@@ -3,7 +3,7 @@ session_start();
 include "admin.php";
 
 if(isset($_GET['act'])){
-    switch (_$GET['act']){
+    switch ($_GET['act']){
         case 'thoat': 
             if(isset($_SESSION['role'])) unset($_SESSION['role']);
             include "index.php";
